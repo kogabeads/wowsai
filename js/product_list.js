99a3393e8370e00c8eafcfea61d5dda23409ab19
kogabeads <kogabeads@gmail.com>
2017-09-19 15:52:50 +0800

    商品详情页面

--- js/product_list.js
+++ js/product_list.js
@@ -1,5 +1,7 @@
 require(["config"],function(){
 	require(["jquery","template","load"],function($,template){
+
+		// ==============商品列表模板==============
 		var html = template("list");
 		$(".product_list").html(html);
 	})
